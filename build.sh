adf8824615b6649f3c6ac8d74905e46cf97675ee
Benjamin Neff <benjamin@coding4coffee.ch>
2011-02-21 22:22:11 +0100

    clean in build.sh eingefuegt und subsections erstellt

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 for theme in AnnArbor Antibes Bergen Berkeley Berlin Boadilla CambridgeUS Copenhagen Darmstadt Dresden Frankfurt Goettingen Hannover Ilmenau JuanLesPins Luebeck Madrid Malmoe Marburg Montpellier PaloAlto Pittsburgh Rochester Singapore Szeged Warsaw ; do 
+	if [ "$1" = "clean" ]; then
+		rm mocking-$theme*
+	else
 		sed "s/%%%%%THEME_PLACEHOLDER_JENKINSBUILD%%%%%/\\\usetheme{$theme}/g" mocking.tex > "mocking-$theme.tex"; 
 		sed "s/%%%%%HANDOUT_PLACEHOLDER_JENKINSBUILD%%%%%/\\\pgfpagesuselayout{4 on 1}[a4paper,border shrink=5mm,landscape]/g" "mocking-$theme.tex" > "mocking-$theme-handout.tex";
 
@@ -7,6 +10,7 @@ for theme in AnnArbor Antibes Bergen Berkeley Berlin Boadilla CambridgeUS Copenh
 		pdflatex --halt-on-error "mocking-$theme.tex"; 
 		pdflatex --halt-on-error "mocking-$theme-handout.tex"; 
 		pdflatex --halt-on-error "mocking-$theme-handout.tex"; 
-	done;
+	fi
+done;
 
 exit 0
