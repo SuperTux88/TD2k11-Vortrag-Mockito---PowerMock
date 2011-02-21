32272a486bb2af5de43c09a5033cef2f7a81ff92
Felix Becker <git@felixbecker.name>
2011-02-21 02:25:36 +0100

    improved

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 for theme in AnnArbor Antibes Bergen Berkeley Berlin Boadilla CambridgeUS Copenhagen Darmstadt Dresden Frankfurt Goettingen Hannover Ilmenau JuanLesPins Luebeck Madrid Malmoe Marburg Montpellier PaloAlto Pittsburgh Rochester Singapore Szeged Warsaw ; do 
-	sed "s/THEME_PLACEHOLDER_JENKINSBUILD/$theme/g" mocking.tex > mocking-$theme.tex; pdflatex --halt-on-error "mocking-$theme.tex"; pdflatex --halt-on-error "mocking-$theme.tex"; done;
+	sed "s/%%%%%THEME_PLACEHOLDER_JENKINSBUILD%%%%%/\\\usetheme{$theme}/g" mocking.tex > mocking-$theme.tex; pdflatex --halt-on-error "mocking-$theme.tex"; pdflatex --halt-on-error "mocking-$theme.tex"; done;
 
 exit 0
