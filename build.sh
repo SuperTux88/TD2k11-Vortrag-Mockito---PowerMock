ce74bb8b2951ef3148b1435cf413ecc682356e79
Benjamin Neff <benjamin@coding4coffee.ch>
2011-03-02 01:43:59 +0100

    build script :)

--- build.sh
+++ build.sh
@@ -3,14 +3,17 @@ for theme in AnnArbor Antibes Bergen Berkeley Berlin Boadilla CambridgeUS Copenh
 	if [ "$1" = "clean" ]; then
 		rm mocking-$theme*
 	else
-		sed "s/%%%%%THEME_PLACEHOLDER_JENKINSBUILD%%%%%/\\\usetheme{$theme}/g" mocking.tex > "mocking-$theme.tex"; 
-		sed "s/%%%%%HANDOUT_PLACEHOLDER_JENKINSBUILD%%%%%/\\\pgfpagesuselayout{4 on 1}[a4paper,border shrink=5mm,landscape]/g" "mocking-$theme.tex" > "mocking-$theme-handout.tex";
+		sed "s/%%%%%THEME_PLACEHOLDER_JENKINSBUILD%%%%%/\\\usetheme{$theme}/g" mocking.tex > "mocking-$theme.tex"
+		sed "s/%%%%%HANDOUT_PLACEHOLDER_JENKINSBUILD%%%%%/\\\pgfpagesuselayout{4 on 1}[a4paper,border shrink=5mm,landscape]/g" "mocking-$theme.tex" > "mocking-$theme-handout.tex"
 
-		pdflatex --halt-on-error "mocking-$theme.tex"; 
-		pdflatex --halt-on-error "mocking-$theme.tex"; 
-		pdflatex --halt-on-error "mocking-$theme-handout.tex"; 
-		pdflatex --halt-on-error "mocking-$theme-handout.tex"; 
+		pdflatex --halt-on-error "mocking-$theme.tex"
+		pdflatex --halt-on-error "mocking-$theme.tex"
+		pdflatex --halt-on-error "mocking-$theme-handout.tex"
+		pdflatex --halt-on-error "mocking-$theme-handout.tex"
 	fi
 done;
+
+cd project
+mvn clean test
 
 exit 0
